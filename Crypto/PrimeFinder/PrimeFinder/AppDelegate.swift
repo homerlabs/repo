681251042cfad43ebc6efdd134e3e3704d170701
homerlabs <homerlabs@mac.com>
2017-11-28 09:54:11 -0800

    Re-did project to fix IB compile warning

---
+++ Crypto/PrimeFinder/PrimeFinder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PrimeFinder
+//
+//  Created by Matthew Homer on 11/28/17.
+//  Copyright © 2017 Matthew Homer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
