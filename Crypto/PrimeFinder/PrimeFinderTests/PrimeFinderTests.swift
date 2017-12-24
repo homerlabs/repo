49681bee0f55105eb006f1365bfd22a13accec8e
homerlabs <homerlabs@mac.com>
2017-12-24 10:11:52 -0800

    Added 2 unit tests

---
+++ Crypto/PrimeFinder/PrimeFinderTests/PrimeFinderTests.swift
@@ -0,0 +1,59 @@
+//
+//  PrimeFinderTests.swift
+//  PrimeFinderTests
+//
+//  Created by Matthew Homer on 12/23/17.
+//  Copyright © 2017 Matthew Homer. All rights reserved.
+//
+
+import XCTest
+
+class PrimeFinderTests: XCTestCase, HLPrimesProtocol {
+
+    var primeFinder: HLPrime?
+    var currentExpectation: XCTestExpectation?
+    
+    let hightestPrime: HLPrimeType = 100000
+    let timeout: TimeInterval = 10
+
+
+    //*************   HLPrimeProtocol     *********************************************************
+    func findPrimesCompleted()  {
+        currentExpectation?.fulfill()
+    }
+    
+    func factorPrimesCompleted()    {
+        currentExpectation?.fulfill()
+    }
+    //*************   HLPrimeProtocol     *********************************************************
+    
+
+    override func setUp() {
+        super.setUp()
+        primeFinder = HLPrime(primeFilePath: "/Volumes/USB64/Primes", modCount: 1000, delegate: self)
+    }
+    
+    override func tearDown() {
+        primeFinder = nil
+        currentExpectation = nil
+        super.tearDown()
+    }
+    
+    func testPrimes() {
+        currentExpectation = expectation(description: "Primes passed")
+        primeFinder!.findPrimes(largestPrime: hightestPrime)
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        wait(for: [currentExpectation!], timeout: timeout)
+        print( "    *********   testPrimes completed" )
+    }
+    
+    func testFactorer() {
+        currentExpectation = expectation(description: "Factorer passed")
+        primeFinder!.findPrimes(largestPrime: hightestPrime)
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        wait(for: [currentExpectation!], timeout: timeout)
+        print( "    *********   testFactorer completed" )
+    }
+    }
+    
+}
