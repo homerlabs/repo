b1780e04fc26ba56ba91da2060405afc09798f8e
homerlabs <homerlabs@mac.com>
2018-03-01 10:44:37 -0800

    Created new project ChineseRemainders

---
+++ ChineseRemainders/ChineseRemainders/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ChineseRemainders
+//
+//  Created by Matthew Homer on 3/1/18.
+//  Copyright © 2018 Matthew Homer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
