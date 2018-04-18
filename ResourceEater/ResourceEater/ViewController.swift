87fb34b9472373def7311a49354e7ef857ea19fa
homerlabs <homerlabs@mac.com>
2018-04-18 11:46:18 -0700

    Created writeToDisk()

--- ResourceEater/ResourceEater/ViewController.swift
+++ ResourceEater/ResourceEater/ViewController.swift
@@ -8,6 +8,9 @@
 
 import Cocoa
 
+let mb = 1048576
+
+
 class ViewController: NSViewController {
 
     @IBOutlet weak var runButton:       NSButton!
@@ -19,21 +22,28 @@ class ViewController: NSViewController {
     let ramSize = 500   //  megabytes
     let diskSize = 500  //  megabytes
     let kTimerInterval = 1.0
-//    var running = false
     var timer: Timer!
-    
+    var count = 0
+    let filename = "ResourceEater"
+
     
     @IBAction func runAction(sender:NSButton)   {
-        print( "ViewController-  runAction-  run state: \(runButton.state)" )
-        
- //       running = !running
+        print( "ViewController-  runAction-  run state: \(runButton.state.rawValue)" )
         
         if runButton.state == .on  {
             runButton.title = "Stop"
+            
+            //  here is 'the loop' where resources are consumed
             timer = Timer.scheduledTimer(withTimeInterval: kTimerInterval, repeats: true) { timer in
+            
+                self.count += 1
             
                 if self.ramButton.state == .on   {
                     self.leakMemory(megabytes: self.ramSize)
+                }
+           
+                if self.diskButton.state == .on   {
+                    self.writeToDisk(megabytes: 1)
                 }
             }
         }
@@ -47,9 +57,40 @@ class ViewController: NSViewController {
         print( "ViewController-  purgeAction" )
         
         ViewController.purgeBuffer()
-        
+        purgeFiles()
     }
     
+
+    func writeToDisk(megabytes: Int)   {
+ //       print( "ViewController-  writeToDisk" )
+ 
+         do {
+             let dir: URL = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).last! as URL
+             let fullFilename = filename + String(count) + ".txt"
+             let url = dir.appendingPathComponent(fullFilename)
+             try fullFilename.write(toFile: url.path, atomically: true, encoding: String.Encoding.utf8)
+         }
+         catch {
+             print("Could not write to file")
+         }
+    }
+    
+    func purgeFiles()   {
+        print( "Purge \(count) files" )
+
+        let dir: URL = FileManager.default.urls(for: .downloadsDirectory, in: .userDomainMask).last! as URL
+        for index in 1...count  {
+            let fullFilename = filename + String(index) + ".txt"
+            let url = dir.appendingPathComponent(fullFilename)
+
+            do  {
+                try FileManager.default.removeItem(atPath: url.path)
+            }
+            catch   {
+                 print("Could not purge file \(fullFilename)")
+            }
+        }
+    }
 
     func leakMemory(megabytes: Int)   {
  //       print( "ViewController-  leakMemory" )
@@ -59,14 +100,13 @@ class ViewController: NSViewController {
 
     static var buffer = [UInt8]()
     static func allocateMemoryOfSize(numberOfMegaBytes: Int) {
-        print("Allocating \(numberOfMegaBytes)MB of memory")
-        let mb = 1048576
         let newBuffer = [UInt8](repeating: 0, count: numberOfMegaBytes * mb)
         buffer += newBuffer
+        print("Allocating \(numberOfMegaBytes)MB of memory    Buf size: \(buffer.count/mb) MBs")
     }
 
     static func purgeBuffer() {
-        print("Purge buffer of size: \(buffer.count)")
+        print("Purge buffer of size: \(buffer.count/mb) MBs")
         buffer.removeAll()
     }
 }
