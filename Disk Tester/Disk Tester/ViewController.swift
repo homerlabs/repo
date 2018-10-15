6e19944387b6480426f98f0011bab9fa8677ce8e
homerlabs <homerlabs@mac.com>
2018-10-15 16:20:05 -0700

    Added security bookmarks

--- Disk Tester/Disk Tester/ViewController.swift
+++ Disk Tester/Disk Tester/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: NSViewController {
 
     let HLSourceFilePathKey      = "SourcePathKey"
     let HLDestinationFilePathKey = "DestinationPathKey"
+    let HLSourceBookmarkKey      = "SourceBookmarkKey"
+    let HLDestinationBookmarkKey = "DestinationBookmarkKey"
     var sourceURL: URL?
     var destinationURL: URL?
 
@@ -20,11 +22,6 @@ class ViewController: NSViewController {
     @IBOutlet weak var destinationPathButton: NSButton!
 
     @IBAction func setSourcePathAction(sender: NSButton) {
-        print( "setSourcePathAction" )
-    }
-
-    @IBAction func setDestinationPathAction(sender: NSButton) {
-
         let savePanel = NSSavePanel();
         savePanel.canCreateDirectories = true;
         savePanel.title = "Set test file path";
@@ -34,23 +31,51 @@ class ViewController: NSViewController {
 
         let i = savePanel.runModal();
         if(i == NSApplication.ModalResponse.OK){
-            savePanel.url!.setBookmarkFor(key: HLDestinationFilePathKey)
+            savePanel.url!.setBookmarkFor(key: HLSourceBookmarkKey)
+            sender.title = savePanel.url!.path
+            UserDefaults.standard.set(savePanel.url!.path, forKey:HLSourceFilePathKey)
+            UserDefaults.standard.synchronize()
+           sourceURL = savePanel.url!.path.getBookmarkFor(key: HLSourceBookmarkKey)
+       }
+        print( "sourceURL: \(String(describing: sourceURL))" )
+    }
+
+    @IBAction func setDestinationPathAction(sender: NSButton) {
+        let savePanel = NSSavePanel();
+        savePanel.canCreateDirectories = true;
+        savePanel.title = "Set test file path";
+        savePanel.nameFieldStringValue = "HLBigTestFile";
+        savePanel.showsTagField = false;
+        savePanel.prompt = "Create";
+
+        let i = savePanel.runModal();
+        if(i == NSApplication.ModalResponse.OK){
+            savePanel.url!.setBookmarkFor(key: HLDestinationBookmarkKey)
             sender.title = savePanel.url!.path
             UserDefaults.standard.set(savePanel.url!.path, forKey:HLDestinationFilePathKey)
-            destinationURL = savePanel.url!.path.getBookmarkFor(key: HLDestinationFilePathKey)
+            UserDefaults.standard.synchronize()
+            destinationURL = savePanel.url!.path.getBookmarkFor(key: HLDestinationBookmarkKey)
        }
+        print( "destinationURL: \(String(describing: destinationURL))" )
     }
 
     override func viewDidDisappear() {
         super.viewDidDisappear()
         print( "ViewController-  viewDidDisappear" )
+        sourceURL?.stopAccessingSecurityScopedResource()
+        destinationURL?.stopAccessingSecurityScopedResource()
         exit(0)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        if let primeFilePath = UserDefaults.standard.string(forKey: HLSourceFilePathKey)  {
+            sourceURL = primeFilePath.getBookmarkFor(key: HLSourceFilePathKey)
+            if sourceURL != nil {
+                sourcePathButton.title = sourceURL!.path
+            }
+        }
     }
 
     override var representedObject: Any? {
