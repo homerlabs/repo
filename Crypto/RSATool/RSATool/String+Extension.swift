3f87d72ddef6cbfedb4ed94af30b4c774a2f31d4
homerlabs <homerlabs@mac.com>
2019-08-17 07:44:08 -0700

    Removed HLDefaultPlaintextFilePathKey, HLDefaultCiphertextFilePathKey, and HLDefaultDeCiphertextFilePathKey

--- Crypto/RSATool/RSATool/String+Extension.swift
+++ Crypto/RSATool/RSATool/String+Extension.swift
@@ -34,25 +34,6 @@ extension String    {
             return (0, 0)
         }
     }
-    
-    func getBookmarkFor(key: String) -> URL?   {
-        var url: URL? = nil
-        
-        if let data = UserDefaults.standard.data(forKey: key)  {
-            do  {
-                var isStale = false
-                    url = try URL(resolvingBookmarkData: data, options: URL.BookmarkResolutionOptions.withSecurityScope, relativeTo: nil, bookmarkDataIsStale: &isStale)
-                    let success = url!.startAccessingSecurityScopedResource()
-
-                    if !success {
-                        print("startAccessingSecurityScopedResource-  success: \(success)")
-                    }
-                } catch {
-                    print("Warning:  Unable to optain security bookmark for key: \(key)!")
-                }
-        }
-        return url
-    }
 }
 
 
@@ -64,18 +45,3 @@ extension Int    {
         return String(format: "%02d:%02d:%02d", hours, mintues, seconds)
     }
 }
-
-
-extension URL    {
-    func setBookmarkFor(key: String) {
-        do  {
-            let data = try self.bookmarkData(options: URL.BookmarkCreationOptions.withSecurityScope,
-                                    includingResourceValuesForKeys: nil, relativeTo: nil)
-            UserDefaults.standard.set(data, forKey:key)
-        }   catch   {
-            print("Warning:  Unable to create security bookmark for key: \(key)!")
-        }
-    }
-}
-
-
