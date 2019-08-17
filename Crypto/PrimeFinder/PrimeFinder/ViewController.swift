dccbe1c22e5739ce5b9dabac0b9716a1ca638ce2
Matthew Homer <homerlabs@mac.com>
2019-08-17 08:18:04 -0700

    Minor change

--- Crypto/PrimeFinder/PrimeFinder/ViewController.swift
+++ Crypto/PrimeFinder/PrimeFinder/ViewController.swift
@@ -53,48 +53,6 @@ class ViewController: NSViewController, NSControlTextEditingDelegate, HLPrimesPr
         }
     }
 
-   func getOpenFilePath(title: String, bookmarkKey: String) -> URL?     {
-    
-        var url: URL?
-        let openPanel = NSOpenPanel();
-        openPanel.canCreateDirectories = true;
-        openPanel.allowedFileTypes = ["txt"];
-        openPanel.showsTagField = false;
-        openPanel.prompt = "Open";
-        openPanel.message = title;
-
-        let i = openPanel.runModal();
-        if(i == NSApplication.ModalResponse.OK) {
-            url = openPanel.url!
-        }
-    
-        print( "getOpenFilePath: \(title),  Bookmark: \(bookmarkKey)  return: \(String(describing: url))" )
-        return url
-    }
-
-   func getSaveFilePath(title: String, fileName: String) -> URL?     {
-    
-        var url: URL?
-        let savePanel = NSSavePanel();
-        savePanel.canCreateDirectories = true;
-        savePanel.title = "PrimeFinder Save Panel";
-        savePanel.nameFieldStringValue = fileName;
-        savePanel.showsTagField = false;
-        savePanel.prompt = "Create";
-        savePanel.message = title;
-        savePanel.nameFieldLabel = "Save As:";
-        savePanel.allowedFileTypes = ["txt"];
-
-        let i = savePanel.runModal();
-        if(i == NSApplication.ModalResponse.OK){
-            url = savePanel.url
-        }
-    
-  //      print( "getSaveFilePath: \(title),return: \(String(describing: url))" )
-        return url
-    }
-
-
     @IBAction func primesStartAction(sender: NSButton) {
         
         if !findPrimesInProgress {
@@ -260,6 +218,47 @@ class ViewController: NSViewController, NSControlTextEditingDelegate, HLPrimesPr
 //  URL Bookmark get and set helpers
 extension ViewController {
 
+    func getOpenFilePath(title: String, bookmarkKey: String) -> URL?     {
+        
+        var url: URL?
+        let openPanel = NSOpenPanel();
+        openPanel.canCreateDirectories = true;
+        openPanel.allowedFileTypes = ["txt"];
+        openPanel.showsTagField = false;
+        openPanel.prompt = "Open";
+        openPanel.message = title;
+        
+        let i = openPanel.runModal();
+        if(i == NSApplication.ModalResponse.OK) {
+            url = openPanel.url
+        }
+        
+        print( "getOpenFilePath: \(title),  Bookmark: \(bookmarkKey)  return: \(String(describing: url))" )
+        return url
+    }
+    
+    func getSaveFilePath(title: String, fileName: String) -> URL?     {
+        
+        var url: URL?
+        let savePanel = NSSavePanel();
+        savePanel.canCreateDirectories = true;
+        savePanel.title = "PrimeFinder Save Panel";
+        savePanel.nameFieldStringValue = fileName;
+        savePanel.showsTagField = false;
+        savePanel.prompt = "Create";
+        savePanel.message = title;
+        savePanel.nameFieldLabel = "Save As:";
+        savePanel.allowedFileTypes = ["txt"];
+        
+        let i = savePanel.runModal();
+        if(i == NSApplication.ModalResponse.OK){
+            url = savePanel.url
+        }
+        
+        //      print( "getSaveFilePath: \(title),return: \(String(describing: url))" )
+        return url
+    }
+    
     func getBookmarkFor(key: String) -> URL?   {
         var url: URL?
         if let data = UserDefaults.standard.data(forKey: key)  {
