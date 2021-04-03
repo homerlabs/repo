2b8e5b820adf79056f38089a0b7f19a2991b8444
homerlabs <homerlabs@mac.com>
2021-04-03 08:35:33 -0700

    Created HLNewsSB (StoryBoard version)

---
+++ dev/News/HLNewsSB/HLNewsSB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HLNewsSB
+//
+//  Created by Matthew Homer on 4/3/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
